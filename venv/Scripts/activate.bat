e316c0d83dc4fcd2561dd5830d8d38f11119a004
Dralax <uityerect@gmail.com>
2022-12-05 23:53:31 +0500

    prodcts desc

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "VIRTUAL_ENV=S:\Ис-409 курсч\salsa\venv"
+set "VIRTUAL_ENV=C:\Users\uitye\PycharmProjects\SALUS\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
